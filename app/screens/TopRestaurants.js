cf102d7981ade4c80126b7b7c01e28c9714e4f0b
bryandms <bryandms95@gmail.com>
2020-05-09 13:52:11 -0600

    Add navigation system

---
+++ app/screens/TopRestaurants.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const TopRestaurants = () => {
+  return (
+    <View>
+      <Text>TopRestaurants</Text>
+    </View>
+  );
+};
+
+export default TopRestaurants;
