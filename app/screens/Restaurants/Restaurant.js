7e67aea03fbf3fcb53c703cf7c1833202869396b
bryandms <bryandms95@gmail.com>
2020-06-19 14:54:35 -0600

    Add restaurant screen

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,15 +1,151 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, ScrollView, Dimensions } from "react-native";
+import { map } from "lodash";
+import { Rating, ListItem, Icon } from "react-native-elements";
+import Loading from "../../components/Loading";
+import CarouselImages from "../../components/CarouselImages";
+import Map from "../../components/Map";
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
+const screenWidth = Dimensions.get("window").width;
+
+const TitleRestaurant = ({ name, description, rating }) => {
+  return (
+    <View style={styles.viewRestaurantTitle}>
+      <View style={{ flexDirection: "row" }}>
+        <Text style={styles.nameRestaurant}>{name}</Text>
+        <Rating
+          style={styles.rating}
+          imageSize={20}
+          readonly
+          startingValue={parseFloat(rating)}
+        />
+      </View>
+      <Text style={styles.descriptionRestaurant}>{description}</Text>
+    </View>
+  );
+};
+
+const RestaurantInfo = ({ location, name, address }) => {
+  const listInfo = [
+    {
+      text: address,
+      iconName: "map-marker",
+      iconType: "material-community",
+      action: null,
+    },
+    {
+      text: "1111-1111",
+      iconName: "phone",
+      iconType: "material-community",
+      action: null,
+    },
+    {
+      text: `${name.trim().toLowerCase()}@example.com`,
+      iconName: "at",
+      iconType: "material-community",
+      action: null,
+    },
+  ];
+
+  return (
+    <View style={styles.viewRestaurantInfo}>
+      <Text style={styles.restaurantInfoTitle}>Restaurant information</Text>
+      <Map location={location} name={name} height={100} />
+      {map(listInfo, (item, index) => (
+        <ListItem
+          key={index}
+          title={item.text}
+          leftIcon={{
+            name: item.iconName,
+            type: item.iconType,
+            color: "#00a680",
+          }}
+          containerStyle={styles.containerListItem}
+        />
+      ))}
+    </View>
+  );
+};
 
 const Restaurant = ({ navigation, route }) => {
-  console.log(navigation, route);
+  const { id, name } = route.params;
+  const [restaurant, setRestaurant] = useState(null);
+  const [rating, setRating] = useState(0);
+
+  navigation.setOptions({ title: name });
+
+  useEffect(() => {
+    db.collection("restaurants")
+      .doc(id)
+      .get()
+      .then((response) => {
+        const data = response.data();
+        data.id = response.id;
+        setRestaurant(data);
+        setRating(data.rating);
+      });
+  }, []);
+
+  if (!restaurant) return <Loading isVisible={true} text="Loading..." />;
+
   return (
-    <View>
-      <Text>Restaurant info</Text>
-    </View>
+    <ScrollView vertical style={styles.viewBody}>
+      <CarouselImages
+        arrayImages={restaurant.images}
+        height={250}
+        width={screenWidth}
+      />
+      <TitleRestaurant
+        name={restaurant.name}
+        description={restaurant.description}
+        rating={rating}
+      />
+      <RestaurantInfo
+        location={restaurant.location}
+        name={restaurant.name}
+        address={restaurant.address}
+      />
+    </ScrollView>
   );
 };
 
 export default Restaurant;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  viewRestaurantTitle: {
+    padding: 15,
+  },
+  nameRestaurant: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  descriptionRestaurant: {
+    marginTop: 5,
+    color: "gray",
+  },
+  rating: {
+    position: "absolute",
+    right: 0,
+  },
+  viewRestaurantInfo: {
+    margin: 15,
+    marginTop: 25,
+  },
+  restaurantInfoTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  containerListItem: {
+    borderBottomColor: "#d8d8d8",
+    borderBottomWidth: 1,
+  },
+});
